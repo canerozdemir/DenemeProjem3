82962a3cf9d8e50c9b63a81aab856a5d096847bd
Caner <ozdemircnr@gmail.com>
2024-01-20 13:44:36 +0300

    Add project files.

---
+++ DenemeProjem3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DenemeProjem3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
